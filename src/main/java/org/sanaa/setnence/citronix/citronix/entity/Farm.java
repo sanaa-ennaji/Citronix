9fd1af5dc70f5eb3c7116d5a10825a3733e8b6a9
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-19 17:01:35 +0100

    farm entity done

--- src/main/java/org/sanaa/setnence/citronix/citronix/entity/Farm.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/entity/Farm.java
@@ -1,32 +1,43 @@
 package org.sanaa.setnence.citronix.citronix.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import jakarta.validation.constraints.PastOrPresent;
+
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "farms")
 public class Farm {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id ;
 
+    @Column(nullable = false)
     private String name ;
 
+    @NotNull
     private String location;
 
-    private String area ;
+    @Column(nullable = false)
+    private double area ;
 
+    @NotNull
+    @PastOrPresent(message = "date should be in the future.")
     private LocalDate createDate ;
+
+    @OneToMany(mappedBy = "farm", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Field> fields ;
 
 
 
