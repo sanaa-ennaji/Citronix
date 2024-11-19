58bd89730375ac8c8ab0a996156cd11ca49f6591
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-19 17:23:23 +0100

    sale class done

--- src/main/java/org/sanaa/setnence/citronix/citronix/entity/Sale.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/entity/Sale.java
@@ -1,14 +1,15 @@
 package org.sanaa.setnence.citronix.citronix.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -16,4 +17,24 @@ public class Sale {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id ;
+    private LocalDate saleDate ;
+
+    private double unitPrice;
+
+    private double quantity ;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "harvest_id", nullable = false)
+    private Harvest harvest;
+
+    @Transient
+    private double revenue;
+
+    @PrePersist
+    @PostLoad
+    public void calculateRevenue() {
+        this.revenue = this.quantity * this.unitPrice;
+    }
+
+
 }
