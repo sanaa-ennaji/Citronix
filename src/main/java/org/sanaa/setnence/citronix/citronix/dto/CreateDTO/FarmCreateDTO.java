18ae34b4ff43c8c7fecf5142555c35cf732e74e5
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-20 11:41:11 +0100

    farm create dto done

--- src/main/java/org/sanaa/setnence/citronix/citronix/dto/CreateDTO/FarmCreateDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/dto/CreateDTO/FarmCreateDTO.java
@@ -2,7 +2,13 @@ package org.sanaa.setnence.citronix.citronix.dto.CreateDTO;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FarmCreateDTO {
     @NotNull
     private String name;
