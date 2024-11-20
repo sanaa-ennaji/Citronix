fb1c6b79f09e85d2027c889b5d6191514ea9bfb0
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-20 11:42:47 +0100

    fiels embedded dto

--- src/main/java/org/sanaa/setnence/citronix/citronix/dto/EmbeddedDTO/FieldEmbbededDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/dto/EmbeddedDTO/FieldEmbbededDTO.java
@@ -1,7 +1,13 @@
 package org.sanaa.setnence.citronix.citronix.dto.EmbeddedDTO;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FieldEmbbededDTO {
     private long id ;
     @NotNull
