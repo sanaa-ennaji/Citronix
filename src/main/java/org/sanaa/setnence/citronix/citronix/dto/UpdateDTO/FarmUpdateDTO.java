6f03c54a38ed3c0521787f6eb308ca2c65c90934
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-20 11:44:52 +0100

    farm update dto done

--- src/main/java/org/sanaa/setnence/citronix/citronix/dto/UpdateDTO/FarmUpdateDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/dto/UpdateDTO/FarmUpdateDTO.java
@@ -1,4 +1,21 @@
 package org.sanaa.setnence.citronix.citronix.dto.UpdateDTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FarmUpdateDTO {
+    @NotNull
+    private String name;
+    @NotBlank
+    private String location;
+    @NotNull
+    private double area;
+    @NotNull
+    private String creationDate;
 }
