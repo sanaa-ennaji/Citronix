d3b64b0420898c3637c00afd4c49247aadace61c
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-20 11:33:10 +0100

    add apiexception[D[D[D[D[D[D[D[E

--- src/main/java/org/sanaa/setnence/citronix/citronix/exception/ApiException.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/exception/ApiException.java
@@ -1,7 +1,18 @@
 package org.sanaa.setnence.citronix.citronix.exception;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class ApiException extends RuntimeException {
-    public ApiException(String message) {
+
+    private String message;
+    private String details;
+
+    public ApiException(String message, String details) {
         super(message);
+        this.message = message;
+        this.details = details;
     }
 }
--- src/main/java/org/sanaa/setnence/citronix/citronix/exception/GlobalHandlerException.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/exception/GlobalHandlerException.java
@@ -1,7 +1,24 @@
 package org.sanaa.setnence.citronix.citronix.exception;
 
-public class GlobalHandlerException extends RuntimeException {
-    public GlobalHandlerException(String message) {
-        super(message);
+import org.sanaa.setnence.citronix.citronix.dto.ErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class GlobalHandlerException  {
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<?> handleApiException(ApiException ex, WebRequest request) {
+        ErrorDTO errorDetails = new ErrorDTO(ex.getMessage(), ex.getDetails());
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorDTO errorDetails = new ErrorDTO(ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
