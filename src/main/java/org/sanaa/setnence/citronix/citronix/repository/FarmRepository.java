2562bfca7409060c7d16ed9d722eae2e080c806c
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-20 12:10:50 +0100

    farm repository

--- src/main/java/org/sanaa/setnence/citronix/citronix/repository/FarmRepository.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/repository/FarmRepository.java
@@ -1,4 +1,7 @@
 package org.sanaa.setnence.citronix.citronix.repository;
 
-public interface FarmRepository {
+import org.sanaa.setnence.citronix.citronix.entity.Farm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FarmRepository extends JpaRepository<Farm, Long> {
 }
