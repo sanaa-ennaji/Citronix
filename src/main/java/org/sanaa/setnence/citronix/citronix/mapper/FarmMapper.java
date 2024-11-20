54bd77dd18b13d7c311b4e086041e2c71e81132b
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-20 14:16:07 +0100

    update diagramme de class

--- src/main/java/org/sanaa/setnence/citronix/citronix/mapper/FarmMapper.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/mapper/FarmMapper.java
@@ -1,7 +1,17 @@
 package org.sanaa.setnence.citronix.citronix.mapper;
 
+import org.mapstruct.factory.Mappers;
+
 public class FarmMapper {
 
 
+    FarmMapper INSTANCE = Mappers.getMapper(FarmMapper.class);
+
+    @Mapping(source = "parentFarmId", target = "parentFarm.id")
+    Farm toEntity(FarmDTO farmDTO);
+
+
+    @Mapping(source = "parentFarm.id", target = "parentFarmId")
+    FarmDTO toDto(Farm farm);
 
 }
