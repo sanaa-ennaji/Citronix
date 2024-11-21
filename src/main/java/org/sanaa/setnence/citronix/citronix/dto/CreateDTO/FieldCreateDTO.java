93ec29fabe74c815ff278f2ccaea7885a81cbad0
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-21 10:48:42 +0100

    validation for the field

--- src/main/java/org/sanaa/setnence/citronix/citronix/dto/CreateDTO/FieldCreateDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/dto/CreateDTO/FieldCreateDTO.java
@@ -1,5 +1,8 @@
 package org.sanaa.setnence.citronix.citronix.dto.CreateDTO;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.sanaa.setnence.citronix.citronix.entity.Farm;
 import org.sanaa.setnence.citronix.citronix.validation.annotation.Exists;
 
@@ -7,8 +10,11 @@ import org.sanaa.setnence.citronix.citronix.validation.annotation.Exists;
 
 public class FieldCreateDTO {
 
+    @NotBlank(message = "Name cannot be blank.")
     private String name;
 
+    @NotNull(message = "Area must not be null.")
+    @Min(value = 0, message = "Area must be greater than or equal to 0.")
     private double area ;
 
     @Exists(entity = Farm.class, message = "farm not found.")
