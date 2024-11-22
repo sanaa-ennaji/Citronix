b0b3e9c625ea3618c222f0efd06ac7f05be31316
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-22 11:23:34 +0100

    removed duplicated farm_id

--- src/main/java/org/sanaa/setnence/citronix/citronix/entity/Field.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/entity/Field.java
@@ -27,8 +27,6 @@ public class Field {
     @JoinColumn(name = "farm_id", nullable = false)
     private Farm farm;
 
-    @Column(name = "farm_id", nullable = false)
-    private Long farmId;
 
     @OneToMany(mappedBy = "field", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
     private List<Tree> trees;
