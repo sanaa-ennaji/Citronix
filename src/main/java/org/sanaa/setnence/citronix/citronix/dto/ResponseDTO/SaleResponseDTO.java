c0da5b9d52fc4b48ff4daf2cada445e3444149c7
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-22 20:47:27 +0100

    response dto for sale

--- src/main/java/org/sanaa/setnence/citronix/citronix/dto/ResponseDTO/SaleResponseDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/dto/ResponseDTO/SaleResponseDTO.java
@@ -1,6 +1,25 @@
 package org.sanaa.setnence.citronix.citronix.dto.ResponseDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SaleResponseDTO {
 
+    private long id;
 
+    private LocalDate saleDate;
+
+    private double unitPrice;
+
+    private double quantity;
+
+    private double revenue;
+
+    private long harvestId;
 }
