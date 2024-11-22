566b6aa7f69f9d9b3feb0fa13c121a5c35aca046
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-22 20:51:52 +0100

    mapper for harvest

--- src/main/java/org/sanaa/setnence/citronix/citronix/mapper/HarvestMapper.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/mapper/HarvestMapper.java
@@ -1,4 +1,27 @@
 package org.sanaa.setnence.citronix.citronix.mapper;
 
-public interface HarvestMapper {
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.sanaa.setnence.citronix.citronix.dto.CreateDTO.HarvestCreateDTO;
+import org.sanaa.setnence.citronix.citronix.dto.ResponseDTO.HarvestResponseDTO;
+import org.sanaa.setnence.citronix.citronix.dto.UpdateDTO.HarvestUpdateDTO;
+import org.sanaa.setnence.citronix.citronix.entity.Harvest;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface HarvestMapper extends GenericMapper<Harvest, HarvestCreateDTO, HarvestUpdateDTO, HarvestResponseDTO> {
+
+    @Override
+    Harvest toEntity(HarvestCreateDTO createDTO);
+
+    @Override
+    HarvestResponseDTO toDTO(Harvest entity);
+
+    @Override
+    List<HarvestResponseDTO> toDTOs(List<Harvest> entities);
+
+    @Override
+    void updateEntityFromDTO(HarvestUpdateDTO updateDTO, @MappingTarget Harvest entity);
 }
