7da1c9a8e1f07c42320ebaa9f5a462f5e996f637
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-22 19:54:42 +0100

    update dto for Tree

--- src/main/java/org/sanaa/setnence/citronix/citronix/dto/UpdateDTO/TreeUpdateDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/dto/UpdateDTO/TreeUpdateDTO.java
@@ -1,4 +1,15 @@
 package org.sanaa.setnence.citronix.citronix.dto.UpdateDTO;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
 public class TreeUpdateDTO {
+    @NotNull(message = "The planting date is required.")
+    @PastOrPresent(message = "The planting date must be in the past or today.")
+    private LocalDate plantingDate;
+
+    @NotNull(message = "Field ID is required.")
+    private long fieldId;
 }
