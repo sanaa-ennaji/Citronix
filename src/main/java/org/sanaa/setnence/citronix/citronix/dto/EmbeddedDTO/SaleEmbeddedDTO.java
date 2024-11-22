fd1fb65bffa7d1a53f076abb277023439fc60ca4
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-22 19:49:18 +0100

    embedded sale dto

--- src/main/java/org/sanaa/setnence/citronix/citronix/dto/EmbeddedDTO/SaleEmbeddedDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/dto/EmbeddedDTO/SaleEmbeddedDTO.java
@@ -1,4 +1,15 @@
 package org.sanaa.setnence.citronix.citronix.dto.EmbeddedDTO;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SaleEmbeddedDTO {
+    private long id;
+    private double unitPrice;
+    private double quantity ;
 }
