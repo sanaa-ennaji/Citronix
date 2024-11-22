fde8ffed0ea2d9f8eea9c36f3ba8dff466202651
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-22 21:18:14 +0100

    sale mapper

--- src/main/java/org/sanaa/setnence/citronix/citronix/mapper/SaleMapper.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/mapper/SaleMapper.java
@@ -1,4 +1,25 @@
 package org.sanaa.setnence.citronix.citronix.mapper;
 
-public interface SaleMapper {
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.sanaa.setnence.citronix.citronix.dto.CreateDTO.SaleCreateDTO;
+import org.sanaa.setnence.citronix.citronix.dto.ResponseDTO.SaleResponseDTO;
+import org.sanaa.setnence.citronix.citronix.dto.UpdateDTO.SaleUpdateDTO;
+import org.sanaa.setnence.citronix.citronix.entity.Sale;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SaleMapper extends GenericMapper<Sale, SaleCreateDTO, SaleUpdateDTO, SaleResponseDTO> {
+    @Override
+    Sale toEntity(SaleCreateDTO createDTO);
+
+    @Override
+    SaleResponseDTO toDTO(Sale entity);
+
+    @Override
+    List<SaleResponseDTO> toDTOs(List<Sale> entities);
+
+    @Override
+    void updateEntityFromDTO(SaleUpdateDTO updateDTO, @MappingTarget Sale entity);
 }
