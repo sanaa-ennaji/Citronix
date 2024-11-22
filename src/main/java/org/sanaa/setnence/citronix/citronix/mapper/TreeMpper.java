f59fc9f27888fbdde681dd33e000b16ff8a1057e
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-22 21:17:15 +0100

    tree mapper

--- src/main/java/org/sanaa/setnence/citronix/citronix/mapper/TreeMpper.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/mapper/TreeMpper.java
@@ -1,5 +1,25 @@
 package org.sanaa.setnence.citronix.citronix.mapper;
 
-public interface TreeMpper {
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.sanaa.setnence.citronix.citronix.dto.CreateDTO.TreeCreateDTO;
+import org.sanaa.setnence.citronix.citronix.dto.ResponseDTO.TreeResponseDTO;
+import org.sanaa.setnence.citronix.citronix.dto.UpdateDTO.TreeUpdateDTO;
+import org.sanaa.setnence.citronix.citronix.entity.Tree;
 
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface TreeMpper extends GenericMapper<Tree, TreeCreateDTO, TreeUpdateDTO, TreeResponseDTO> {
+    @Override
+    Tree toEntity(TreeCreateDTO createDTO);
+
+    @Override
+    TreeResponseDTO toDTO(Tree entity);
+
+    @Override
+    List<TreeResponseDTO> toDTOs(List<Tree> entities);
+
+    @Override
+    void updateEntityFromDTO(TreeUpdateDTO updateDTO, @MappingTarget Tree entity);
 }
