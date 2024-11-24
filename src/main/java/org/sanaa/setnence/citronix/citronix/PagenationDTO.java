5347d3c50a780df980339e98e904e32f81b3b5b1
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-24 14:20:55 +0100

    dto for pagenation

--- src/main/java/org/sanaa/setnence/citronix/citronix/PagenationDTO.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/PagenationDTO.java
@@ -1,4 +1,21 @@
 package org.sanaa.setnence.citronix.citronix;
 
-public class PagenationDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PagenationDTO<T> {
+
+    private List<T> content ;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last ;
+
 }
