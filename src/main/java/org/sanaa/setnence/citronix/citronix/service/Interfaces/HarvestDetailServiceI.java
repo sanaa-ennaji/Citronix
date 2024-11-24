df76aa510a5e3ce072d9fafff9cdea8836a828fc
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-24 14:04:22 +0100

    harvestDetail Service Interface

---
+++ src/main/java/org/sanaa/setnence/citronix/citronix/service/Interfaces/HarvestDetailServiceI.java
@@ -0,0 +1,9 @@
+package org.sanaa.setnence.citronix.citronix.service.Interfaces;
+
+import org.sanaa.setnence.citronix.citronix.dto.CreateDTO.HarvestDetailCreateDTO;
+import org.sanaa.setnence.citronix.citronix.dto.ResponseDTO.HarvestDetailResponseDTO;
+import org.sanaa.setnence.citronix.citronix.dto.UpdateDTO.HarvestDetailUpdateDTO;
+import org.sanaa.setnence.citronix.citronix.entity.HarvestDetail;
+
+public interface HarvestDetailServiceI extends GenericServiceI<HarvestDetail, HarvestDetailCreateDTO, HarvestDetailUpdateDTO, HarvestDetailResponseDTO>{
+}
