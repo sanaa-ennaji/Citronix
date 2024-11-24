9061d270201552a3e6439a0168a9cc2aec3f2987
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-24 14:00:53 +0100

    Tree service interface

--- src/main/java/org/sanaa/setnence/citronix/citronix/service/Interfaces/TreeServiceI.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/service/Interfaces/TreeServiceI.java
@@ -1,4 +1,9 @@
 package org.sanaa.setnence.citronix.citronix.service.Interfaces;
 
-public interface TreeServiceI {
+import org.sanaa.setnence.citronix.citronix.dto.CreateDTO.TreeCreateDTO;
+import org.sanaa.setnence.citronix.citronix.dto.ResponseDTO.TreeResponseDTO;
+import org.sanaa.setnence.citronix.citronix.dto.UpdateDTO.TreeUpdateDTO;
+import org.sanaa.setnence.citronix.citronix.entity.Tree;
+
+public interface TreeServiceI  extends GenericServiceI<Tree, TreeCreateDTO, TreeUpdateDTO, TreeResponseDTO>{
 }
