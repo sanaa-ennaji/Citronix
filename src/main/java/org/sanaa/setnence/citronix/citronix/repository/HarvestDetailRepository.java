f0057fe351c1d7bd5e2ee809eb910f2a831473f4
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-24 13:30:37 +0100

    harvestDetail repository

--- src/main/java/org/sanaa/setnence/citronix/citronix/repository/HarvestDetailRepository.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/repository/HarvestDetailRepository.java
@@ -1,4 +1,8 @@
 package org.sanaa.setnence.citronix.citronix.repository;
 
-public interface HarvestDetailRepository {
+import org.sanaa.setnence.citronix.citronix.entity.HarvestDetail;
+import org.sanaa.setnence.citronix.citronix.entity.embedded.EmbeddedHarvestDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HarvestDetailRepository extends JpaRepository<HarvestDetail, EmbeddedHarvestDetail> {
 }
