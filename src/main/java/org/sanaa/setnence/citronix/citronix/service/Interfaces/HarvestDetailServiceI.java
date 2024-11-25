6b8da34215d462571e2814b1a99f338a0edb8481
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-11-25 01:30:39 +0100

    harvest details service interface

--- src/main/java/org/sanaa/setnence/citronix/citronix/service/Interfaces/HarvestDetailServiceI.java
+++ src/main/java/org/sanaa/setnence/citronix/citronix/service/Interfaces/HarvestDetailServiceI.java
@@ -3,7 +3,15 @@ package org.sanaa.setnence.citronix.citronix.service.Interfaces;
 import org.sanaa.setnence.citronix.citronix.dto.CreateDTO.HarvestDetailCreateDTO;
 import org.sanaa.setnence.citronix.citronix.dto.ResponseDTO.HarvestDetailResponseDTO;
 import org.sanaa.setnence.citronix.citronix.dto.UpdateDTO.HarvestDetailUpdateDTO;
-import org.sanaa.setnence.citronix.citronix.entity.HarvestDetail;
+import org.sanaa.setnence.citronix.citronix.entity.embedded.EmbeddedHarvestDetail;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface HarvestDetailServiceI {
+    HarvestDetailResponseDTO create(HarvestDetailCreateDTO createDTO);
+    HarvestDetailResponseDTO update(EmbeddedHarvestDetail id, HarvestDetailUpdateDTO updateDTO);
+    Optional<HarvestDetailResponseDTO> findById(EmbeddedHarvestDetail id);
+    List<HarvestDetailResponseDTO> findAll();
+    void delete(EmbeddedHarvestDetail id);
 }
